55305d27efd6fd51d43d91676148ed14e135fcec
20Lush <57083746+20Lush@users.noreply.github.com>
2023-02-17 13:20:34 -0800

    Add files via upload

---
+++ payload-tester.ino
@@ -0,0 +1,47 @@
+#include <Servo.h>
+
+Servo door, drop;
+int btn1 = 12, btn2 = 11;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  door.attach(A0);
+  drop.attach(A1);
+  pinMode(btn1, INPUT_PULLUP);
+  pinMode(btn2, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(!digitalRead(btn1)){
+    door.write(servoConvert(90));
+    delay(2000);
+    drop.write(servoConvert(270));
+    delay(2000);
+  }
+
+  else if(!digitalRead(btn2)){
+    drop.write(servoConvert(0));
+    delay(2000);
+    door.write(servoConvert(0));
+    delay(2000);
+  }
+
+  else{
+    door.write(servoConvert(0));
+    drop.write(servoConvert(0));
+  }
+  
+
+
+}
+
+int servoConvert(int angle){
+
+  return map(angle, 0, 270, 0, 180);
+
+}
