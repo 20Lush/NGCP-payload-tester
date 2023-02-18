0c7d29f6695ce12c5422b41ee5ef4fcbc30b806f
20Lush <57083746+20Lush@users.noreply.github.com>
2023-02-18 13:50:04 -0800

    Add files via upload

--- payload-tester.ino
+++ payload-tester.ino
@@ -1,48 +1,72 @@
 #include <Servo.h>
 
-Servo door, drop;
+Servo door, drop1, drop2;
 int btn1 = 12, btn2 = 11;
 
-bool initServos = false;
+bool initServos = true, diagnostic = true;
 
 
 void setup() {
   // put your setup code here, to run once:
 
+  Serial.begin(9600);
+  delay(3000);
+
   door.attach(A0);
-  drop.attach(A1);
+  drop1.attach(A1);
+  drop2.attach(A2);
   pinMode(btn1, INPUT_PULLUP);
   pinMode(btn2, INPUT_PULLUP);
 
   if(initServos){
 
     door.writeMicroseconds(microSecondsConvert(0));
-    drop.writeMicroseconds(microSecondsConvert(0));
+    drop1.writeMicroseconds(microSecondsConvert(0));
+    drop2.writeMicroseconds(microSecondsConvert(0));
 
   }
+
+  Serial.println("==");
+  Serial.println("----------- initialized servos -----------");
+  Serial.print("door: ");
+  Serial.print(door.read());
+  Serial.print(" drop1: ");
+  Serial.print(drop1.read());
+  Serial.print(" drop2: ");
+  Serial.println(drop2.read());
+  Serial.println("------------------------------------------");
+  Serial.println("==");
+
+  delay(3000);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if(!digitalRead(btn1)){
-    door.write(servoConvert(90));
-    delay(2000);
-    drop.write(servoConvert(270));
-    delay(2000);
-  }
+  if(!diagnostic){
+    if(!digitalRead(btn1)){ // run open
 
-  else if(!digitalRead(btn2)){
-    drop.write(servoConvert(0));
-    delay(2000);
-    door.write(servoConvert(0));
-    delay(2000);
+    }
+
+    else if(!digitalRead(btn2)){ // reset to close
+
+    }
+
+    else{ // default the values
+
+    }
   }
 
   else{
-    door.write(servoConvert(0));
-    drop.write(servoConvert(0));
+
+    Serial.print("door: ");
+    Serial.print(door.read());
+    Serial.print(" drop1: ");
+    Serial.print(drop1.read());
+    Serial.print(" drop2: ");
+    Serial.println(drop2.read());
+
   }
   
 
@@ -58,5 +82,26 @@ int servoConvert(int angle){
 int microSecondsConvert(int angle){
 
   return map(angle, 0, 270, 544, 2400);
+
+}
+
+void trueAngleTo(Servo servo, int angle){ //0 - 180 shifted for 270deg servos
+
+  angle = microSecondsConvert(angle);
+  servo.writeMicroseconds(angle);
+  
+}
+
+void feedForwardSlew(Servo servo, int angle, int step = 1){
+
+  int curr = servo.read();
+  int delta = angle - curr;
+  int pol = delta/(abs(delta));
+
+  for(int i = 0; i < abs(delta); i+step){
+
+    servo.writeMicroseconds(microSecondsConvert(curr + (i*pol)));
+
+  }
 
 }
